97cb0f8375a1189cd87f5c5e2472edc581bf81f6
Adam Milligan <adam@buildgroundwork.com>
2018-05-13 19:27:31 -0400

    Replace obsolete method call
    rubocop: Lint/UriEscapeUnescape

--- lib/onfleet-ruby/actions/find.rb
+++ lib/onfleet-ruby/actions/find.rb
@@ -3,7 +3,7 @@ module Onfleet
     module Find
       module ClassMethods
         def find field, search_term
-          encoded_term = URI::encode(search_term)
+          encoded_term = URI.encode_www_form_component(search_term)
           api_url = "#{self.api_url}/#{field}/#{encoded_term}"
           response = Onfleet.request(api_url, :get, search_term)
           Util.constantize("#{self}").new(response)

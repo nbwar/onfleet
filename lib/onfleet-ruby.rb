cd48ba890e0a1a57b79a279f22b9c40a8ecc4325
Adam Milligan <adam@buildgroundwork.com>
2018-05-13 19:19:16 -0400

    Make private methods actually private
    rubocop: Lint/IneffectiveAccessModifier

--- lib/onfleet-ruby.rb
+++ lib/onfleet-ruby.rb
@@ -40,64 +40,64 @@ module Onfleet
 
   class << self
     attr_accessor :api_key, :base_url
-  end
 
-  def self.request api_url, method, params = {}
-    raise AuthenticationError.new("Set your API Key using Onfleet.api_key = <API_KEY>") unless @api_key
+    def request api_url, method, params = {}
+      raise AuthenticationError.new("Set your API Key using Onfleet.api_key = <API_KEY>") unless @api_key
 
-    begin
-      response = RestClient::Request.execute(method: method, url: self.base_url + api_url, payload: params.to_json, headers: self.request_headers)
+      begin
+        response = RestClient::Request.execute(method: method, url: base_url + api_url, payload: params.to_json, headers: request_headers)
 
-      if response != ''
-        JSON.parse(response)
-      end
-    rescue RestClient::ExceptionWithResponse => e
-      if (response_code = e.http_code) and (response_body = e.http_body)
-        handle_api_error(response_code, JSON.parse(response_body))
-      else
+        if response != ''
+          JSON.parse(response)
+        end
+      rescue RestClient::ExceptionWithResponse => e
+        if (response_code = e.http_code) and (response_body = e.http_body)
+          handle_api_error(response_code, JSON.parse(response_body))
+        else
+          handle_restclient_error(e)
+        end
+      rescue RestClient::Exception, Errno::ECONNREFUSED => e
         handle_restclient_error(e)
       end
-    rescue RestClient::Exception, Errno::ECONNREFUSED => e
-      handle_restclient_error(e)
-    end
-  end
-
-  private
-
-  def self.request_headers
-    {
-      Authorization: "Basic #{self.encoded_api_key}",
-      content_type: :json,
-      accept: :json
-    }
-  end
-
-  def self.encoded_api_key
-    @encoded_api_key ||= Base64.urlsafe_encode64(@api_key)
-  end
-
-  def self.handle_api_error code, body
-    case code
-    when 400, 404
-      raise InvalidRequestError.new(body["message"])
-    when 401
-      raise AuthenticationError.new(body["message"])
-    else
-      raise OnfleetError.new(body["message"])
-    end
-  end
-
-  def self.handle_restclient_error e
-    case e
-    when RestClient::RequestTimeout
-      message = "Could not connect to Onfleet. Check your internet connection and try again."
-    when RestClient::ServerBrokeConnection
-      message = "The connetion with onfleet terminated before the request completed. Please try again."
-    else
-      message = "There was a problem connection with Onfleet. Please try again. If the problem persists contact contact@onfleet.com"
     end
 
-    raise ConnectionError.new(message)
+    private
+
+    def request_headers
+      {
+        Authorization: "Basic #{encoded_api_key}",
+        content_type: :json,
+        accept: :json
+      }
+    end
+
+    def encoded_api_key
+      @encoded_api_key ||= Base64.urlsafe_encode64(api_key)
+    end
+
+    def handle_api_error code, body
+      case code
+      when 400, 404
+        raise InvalidRequestError.new(body["message"])
+      when 401
+        raise AuthenticationError.new(body["message"])
+      else
+        raise OnfleetError.new(body["message"])
+      end
+    end
+
+    def handle_restclient_error e
+      case e
+      when RestClient::RequestTimeout
+        message = "Could not connect to Onfleet. Check your internet connection and try again."
+      when RestClient::ServerBrokeConnection
+        message = "The connetion with onfleet terminated before the request completed. Please try again."
+      else
+        message = "There was a problem connection with Onfleet. Please try again. If the problem persists contact contact@onfleet.com"
+      end
+
+      raise ConnectionError.new(message)
+    end
   end
 end
 
